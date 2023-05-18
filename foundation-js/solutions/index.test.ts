1b11d8540c116acd02a419611ea2b975697b1e9f
artworkk <prem@cleverse.com>
2023-05-19 03:21:52 +0700

    refactor: use tuple

--- foundation-js/solutions/index.test.ts
+++ foundation-js/solutions/index.test.ts
@@ -70,31 +70,35 @@ describe("prime", () => {
     // 2, 3, 5, 7, 11, 13, 17
     // The test case input needs to be in an array
     // to simplify type system for the test code
-    [
-      [[1], [2]],
-      [[2], [2, 3]],
-      [[3], [2, 3, 5]],
-      [[4], [2, 3, 5, 7]],
-      [[5], [2, 3, 5, 7, 11]],
-      [[6], [2, 3, 5, 7, 11, 13]],
-      [[7], [2, 3, 5, 7, 11, 13, 17]],
-    ].forEach((test) => {
-      expect(prime(test[0][0])).toEqual(test[1]);
+    const tests: [number, number[]][] = [
+      [1, [2]],
+      [2, [2, 3]],
+      [3, [2, 3, 5]],
+      [4, [2, 3, 5, 7]],
+      [5, [2, 3, 5, 7, 11]],
+      [6, [2, 3, 5, 7, 11, 13]],
+      [7, [2, 3, 5, 7, 11, 13, 17]],
+    ];
+
+    tests.forEach((test) => {
+      expect(prime(test[0])).toStrictEqual(test[1]);
     });
   });
 });
 
 describe("drawDown", () => {
   it("unexpected value", () => {
-    [
-      [[1, 2], [0]],
-      [[1, 4, 2, 1], [3]],
-      [[1, 10, 12, 14, 7, 4, 20, 9], [11]],
-      [[100, 90, 80, 10, 110, 10, 1], [109]],
-      [[200, 90, 80, 10, 110, 10, 1], [199]],
-      [[7, 8, 9, 1, 10, 8, 6, 11, 200], [8]],
-    ].forEach((test) => {
-      expect(drawDown(test[0])).toEqual(test[1][0]);
+    const tests: [number[], number][] = [
+      [[1, 2], 0],
+      [[1, 4, 2, 1], 3],
+      [[1, 10, 12, 14, 7, 4, 20, 9], 11],
+      [[100, 90, 80, 10, 110, 10, 1], 109],
+      [[200, 90, 80, 10, 110, 10, 1], 199],
+      [[7, 8, 9, 1, 10, 8, 6, 11, 200], 8],
+    ];
+
+    tests.forEach((test) => {
+      expect(drawDown(test[0])).toStrictEqual(test[1]);
     });
   });
 });
